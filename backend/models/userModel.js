3c08d0348c4b9f6ba4d48d03487afed08f833266
Benni <Benni>
2024-09-10 16:24:32 +0200

    models

---
+++ backend/models/userModel.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const userSchema = new mongoose.Schema({
+    username : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    password : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    classTeam : {
+        type : String,
+        required : true
+    },
+});
+
+// Password hashing vor dem speichern
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  });
+
+const user = mongoose.model('user', userSchema);
+
+export default user;
