21c21d8e3e6961fdae7f1bd1d9279e6f827120ca
Benni <Benni>
2024-09-11 10:58:33 +0200

    env

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // cors anfragen vom Frontend erlauben
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'http://localhost:5173', // Frontend URL
   optionsSuccessStatus: 200
 };
 
