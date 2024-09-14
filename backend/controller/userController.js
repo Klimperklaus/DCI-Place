650f7e9c02f736de7564d72880b53634c34399d0
BenniDci <benjamin.schult@dci-student.org>
2024-09-14 14:34:36 +0200

    Docker Setup

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,5 +1,5 @@
 import User from "../models/userModel.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 const handleError = (res, status, msg) => res.status(status).json({ msg });
