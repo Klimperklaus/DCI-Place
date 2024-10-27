0b2a1a280715697e9e6badea59d96f50897b97ef
Swift-TuttleA3 <r.panek888@gmail.com>
2024-10-27 11:07:17 +0100

    fixed mongo connect and pixelplacing, update on structure needed

--- frontend/src/components/WebSocketClient.jsx
+++ frontend/src/components/WebSocketClient.jsx
@@ -1,3 +1,4 @@
+
 import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import Cookies from "js-cookie";
@@ -74,81 +75,3 @@ WebSocketClient.propTypes = {
 };
 
 export default WebSocketClient;
-/*
-import { useEffect, useState } from "react";
-import PropTypes from "prop-types";
-import Cookies from "js-cookie";
-
-const WebSocketClient = ({
-  setWs,
-  setConnectionStatus,
-  setMessages,
-  setError,
-}) => {
-  const [localWs, setLocalWs] = useState(null);
-
-  useEffect(() => {
-    if (localWs) {
-      localWs.onopen = () => {
-        console.log("WebSocket connection established");
-        setConnectionStatus("Connected");
-      };
-      localWs.onmessage = (event) => {
-        try {
-          const data = JSON.parse(event.data);
-          console.log("Data received from WebSocket:", data);
-          if (data.error) {
-            console.error("Server error: ", data.error);
-            setError(data.error);
-          } else {
-            setMessages((prevMessages) => [...prevMessages, data]);
-          }
-        } catch (error) {
-          console.error("Error parsing message from server: ", error);
-        }
-      };
-      localWs.onerror = (error) => {
-        console.error("WebSocket error: ", error);
-      };
-      localWs.onclose = () => {
-        console.log("WebSocket connection closed");
-        setConnectionStatus("Disconnected");
-      };
-    }
-  }, [localWs, setConnectionStatus, setError, setMessages]);
-
-  const connectWebSocket = () => {
-    const token = Cookies.get("token_js");
-    if (!token) {
-      console.error("Kein Token im lokalen Speicher gefunden.");
-      return;
-    }
-    if (localWs) {
-      localWs.close();
-      setLocalWs(null);
-      setWs(null);
-      console.log('WebSocket connection closed');
-    } else {
-      const newWs = new WebSocket(`ws://localhost:3131?token=${token}`);
-      setLocalWs(newWs);
-      setWs(newWs);
-      console.log('WebSocket connection initiated');
-    }
-  };
-
-  return (
-    <button onClick={connectWebSocket}>
-      {localWs ? "Disconnect WebSocket" : "Connect WebSocket"}
-    </button>
-  );
-};
-
-WebSocketClient.propTypes = {
-  setWs: PropTypes.func.isRequired,
-  setConnectionStatus: PropTypes.func.isRequired,
-  setMessages: PropTypes.func.isRequired,
-  setError: PropTypes.func.isRequired,
-};
-
-export default WebSocketClient;
-*/
