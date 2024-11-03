f7e63c5f7be057ce3108dfbb3a7833f4a704025b
Klimperklaus <steven.mueller@dci-student.org>
2024-11-03 16:14:10 +0100

    parallax completed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,16 @@
-import Parallax from "../utilities/Parallax.jsx"
+import Parallax from "../utilities/Parallax.jsx";
+import "../styles/Home.scss";
 
 const Home = () => {
-    return (
-        <div className="flex justify-center items-center w-full h-full">
-            <Parallax />
-            <div className="flex flex-col justify-center items-center absolute">
-                <h1>hello</h1>
-                <p>was ne sau</p>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="wrapper flex justify-center items-center w-full h-full">
+      <Parallax />
+      {/* <div className="flex flex-col justify-center items-center w-3/4 h-3/4 border border-black absolute">
+        <h1>hello</h1>
+        <p>was ne sau</p>
+      </div> */}
+    </div>
+  );
+};
 
 export default Home;
--- frontend/src/utilities/Parallax.jsx
+++ frontend/src/utilities/Parallax.jsx
@@ -1,52 +1,40 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import {
   MouseParallaxContainer,
   MouseParallaxChild,
 } from "react-parallax-mouse";
 
-const pixelObj = {
-  pixel_6: {
+const pixelArr = [
+  {
     size: 6,
     factor: 0.06,
   },
-  pixel_12: {
+  {
     size: 12,
     factor: 0.12,
   },
-  pixel_18: {
+  {
     size: 18,
     factor: 0.18,
   },
-  pixel_24: {
+  {
     size: 24,
     factor: 0.24,
   },
-  pixel_30: {
+  {
     size: 30,
     factor: 0.3,
   },
-};
+];
 
-// function calcPixelSize() {
-//   return Math.floor(Math.random() * (30 + 10 + 1) + 10);
-// }
+let renderArr = [];
+for (let i = 0; i < 50; i++) {
+  renderArr.push(pixelArr[Math.floor(Math.random() * pixelArr.length)]);
+}
 
-// function calcFactor(pixelSize) {
-//   let factor = (Math.random() / 5).toFixed(2);
-//   if (pixelSize >= 20 && factor < 0.2) {
-//     while (factor > 0.05) {
-//       factor = (Math.random() / 5).toFixed(2);
-//     }
-//   }
-//   return factor;
-// }
+renderArr.sort((a, b) => a.size - b.size);
 
 const Parallax = () => {
-  const [screenSize, setScreenSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
-
   const colors = [
     "rgba(113, 124, 179, 1)",
     "rgba(209, 100, 91, 1)",
@@ -54,100 +42,41 @@ const Parallax = () => {
     "rgba(240, 154, 100, 1)",
     "rgba(50, 150, 148, 1)",
     "rgba(148, 93, 78, 1)",
-    "rgba(235, 140, 128, 1)"
-  ]
+    "rgba(235, 140, 128, 1)",
+  ];
 
-  const far_away_pixels = [
-    //
-  ]
-
-  useEffect(() => {
-    const handleResize = () => {
-      setScreenSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
+  function getRandomColor() {
+    const randNum = Math.floor(Math.random() * colors.length);
+    return colors[randNum];
+  }
 
   return (
     <>
-      {/** @TODO set pixels with parallax */}
       <MouseParallaxContainer
         containerStyle={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
           width: "100dvw",
           height: "100dvh",
         }}
       >
-        <MouseParallaxChild
-          factorX={pixelObj.pixel_6.factor}
-          factorY={pixelObj.pixel_6.factor}
-        >
-          <div
-            style={{
-              width: `${pixelObj.pixel_6.size}px`,
-              height: `${pixelObj.pixel_6.size}px`,
-              backgroundColor: "cyan",
-            }}
-          ></div>
-        </MouseParallaxChild>
-        <MouseParallaxChild
-          factorX={pixelObj.pixel_12.factor}
-          factorY={pixelObj.pixel_12.factor}
-        >
-          <div
-            style={{
-              width: `${pixelObj.pixel_12.size}px`,
-              height: `${pixelObj.pixel_12.size}px`,
-              backgroundColor: "blue",
-            }}
-          ></div>
-        </MouseParallaxChild>
-        <MouseParallaxChild
-          factorX={pixelObj.pixel_18.factor}
-          factorY={pixelObj.pixel_18.factor}
-        >
-          <div
-            style={{
-              width: `${pixelObj.pixel_18.size}px`,
-              height: `${pixelObj.pixel_18.size}px`,
-              backgroundColor: "green",
-            }}
-          ></div>
-        </MouseParallaxChild>
-        <MouseParallaxChild
-          factorX={pixelObj.pixel_24.factor}
-          factorY={pixelObj.pixel_24.factor}
-        >
-          <div
-            style={{
-              width: `${pixelObj.pixel_24.size}px`,
-              height: `${pixelObj.pixel_24.size}px`,
-              backgroundColor: "black",
-            }}
-          ></div>
-        </MouseParallaxChild>
-        <MouseParallaxChild
-          factorX={pixelObj.pixel_30.factor}
-          factorY={pixelObj.pixel_30.factor}
-        >
-          <div
-            style={{
-              width: `${pixelObj.pixel_30.size}px`,
-              height: `${pixelObj.pixel_30.size}px`,
-              backgroundColor: "orange",
-            }}
-          ></div>
-        </MouseParallaxChild>
+        {renderArr.map((pixel, index) => (
+          <MouseParallaxChild
+            key={`childComp ${index}`}
+            factorX={pixel.factor}
+            factorY={pixel.factor}
+          >
+            <div
+              key={`div ${index}`}
+              style={{
+                width: `${pixel.size}px`,
+                height: `${pixel.size}px`,
+                backgroundColor: `${getRandomColor(colors)}`,
+                position: "absolute",
+                top: `${Math.floor(Math.random() * window.innerHeight)}px`,
+                left: `${Math.floor(Math.random() * window.innerWidth)}px`,
+              }}
+            ></div>
+          </MouseParallaxChild>
+        ))}
       </MouseParallaxContainer>
     </>
   );
